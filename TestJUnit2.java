34f5bee28e4bfbf3d36b16fe6b2034e35faa521c
VaibhavP189 <34150991+VaibhavP189@users.noreply.github.com>
2018-03-09 22:37:23 +0530

    Add files via upload

---
+++ TestJUnit2.java
@@ -0,0 +1,13 @@
+package TestJUnit2;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJUnit2 {
+
+	@Test
+	public void testAdd() {
+String str= "Junit is working fine";
+assertEquals("Junit is working fine",str);
+
+	}
+}
